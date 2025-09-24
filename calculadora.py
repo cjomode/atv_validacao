572aea90c73163bb904c3330fc6142c154e00046
Juliana Moreira <oliveiramjuliana22@gmail.com>
2025-09-23 23:09:30 -0300

    new tests

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,30 @@
 class Calculadora:
 
+    LIMITE_MAXIMO = 9999999
+
     def soma(a, b):
         resultado = a + b
+        Calculadora._verificar_overflow(resultado)
         return resultado
 
     def subtracao(a, b):
         resultado = a - b
+        Calculadora._verificar_overflow(resultado)
         return resultado
 
     def multiplicacao(a, b):
         resultado = a * b
+        Calculadora._verificar_overflow(resultado)
         return resultado
 
     def divisao(a, b):
         if b == 0:
             raise ZeroDivisionError("Erro: Divisão por zero")
         resultado = a / b
+        Calculadora._verificar_overflow(resultado)
         return resultado
+    
+    def _verificar_overflow(valor):
+        if abs(valor) > Calculadora.LIMITE_MAXIMO:
+            raise OverflowError("Erro: Overflow")
+
--- test_calculadora.py
+++ test_calculadora.py
@@ -3,19 +3,23 @@ from calculadora import Calculadora
 
 class TestCalculadora(unittest.TestCase):
     def test_soma(self):
-        self.assertEqual(Calculadora.soma(3, 2), 5)
-        self.assertEqual(Calculadora.soma(-1, 1), 0)
-        self.assertEqual(Calculadora.soma(-5, -7), -12)
+        #partição de equivalência
+        self.assertEqual(Calculadora.soma(3, 2), 5) #positivos
+        self.assertEqual(Calculadora.soma(-1, 1), 0) #positivo e negativo
+        self.assertEqual(Calculadora.soma(-5, -7), -12) #negativos
+        self.assertEqual(Calculadora.soma(5.5, 2.3),7.8) #decimais
 
     def test_subtracao(self):
         self.assertEqual(Calculadora.subtracao(10, 5), 5)
         self.assertEqual(Calculadora.subtracao(0, 5), -5)
         self.assertEqual(Calculadora.subtracao(-3, -2), -1)
+        self.assertEqual(Calculadora.subtracao(9.1,2.1),7)
 
     def test_multiplicacao(self):
         self.assertEqual(Calculadora.multiplicacao(4, 5), 20)
         self.assertEqual(Calculadora.multiplicacao(-2, 3), -6)
         self.assertEqual(Calculadora.multiplicacao(0, 100), 0)
+        self.assertEqual(Calculadora.multiplicacao(7.8,1.6),12.48)
 
     def test_divisao(self):
         self.assertEqual(Calculadora.divisao(10, 2), 5)
@@ -23,6 +27,22 @@ class TestCalculadora(unittest.TestCase):
 
         with self.assertRaises(ZeroDivisionError):
             Calculadora.divisao(5, 0)
+
+    def test_valor_limite(self):
+        self.assertEqual(Calculadora.soma(9999990, 9), 9999999)  #limite
+        with self.assertRaises(OverflowError):
+            Calculadora.soma(9999999, 9999999 + 1)  #ultrapassa o limite
+
+    def test_tabela_decisao(self):
+        self.assertEqual(Calculadora.soma(15, 0), 15)
+
+    #Transição de Estados 
+    def test_transicao_estados(self):
+        resultado = Calculadora.soma(5, 5)     #estado inicial
+        resultado = Calculadora.multiplicacao(resultado, 2)  #novo estado
+        resultado = Calculadora.subtracao(resultado, 5)  #transição
+        self.assertEqual(resultado, 15)
+
 
 if __name__ == '__main__':
     unittest.main()
